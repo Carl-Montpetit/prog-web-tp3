4ab3d9470443c818317e4bae29e89856f68e8eab
Carl <montpetit.carl@icloud.com>
2021-08-03 19:42:50 -0400

    added some test + comments to explain the code

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
-
+// Component definition
 @Component({
+  // the name of the component when called in another file (ex: <h1></h1>)
   selector: 'app-server',
+  //--------------------------------------------------------------------------------------------------------------------
+  // external template (html sheet) linked to this component
   templateUrl: './server.component.html'
+  //--------------------------------------------------------------------------------------------------------------------
+  // external style sheet (CSS) file linked to this component
+  // nothing for now
 })
 export class ServerComponent {
+  serverID: number = 10;
+  serverStatus: string = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+// Component definition
 @Component({
+  // both are the same thing
+  // selector: '[app-servers]',
+  // selector: '.app=servers',
+  //--------------------------------------------------------------------------------------------------------------------
+  // the name of the component when called in another file (ex: <h1></h1>)
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  //--------------------------------------------------------------------------------------------------------------------
+  // internal template (html sheet) without passing to another file
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  //--------------------------------------------------------------------------------------------------------------------
+  // external template (html sheet) linked to this component
+  templateUrl: 'servers.component.html',
+  //--------------------------------------------------------------------------------------------------------------------
+  // style sheet (CSS) file linked to this component
   styleUrls: ['./servers.component.css']
+  //--------------------------------------------------------------------------------------------------------------------
 })
 export class ServersComponent implements OnInit {
 
