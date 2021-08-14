2f94e88c9f3671e57ef2f8f14dc91050d28aca9c
Carl <montpetit.carl@icloud.com>
2021-08-14 15:45:22 -0400

    BIG update for programs⇧⚠️

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { ProgramsComponent } from './programs/programs.component';
-import { ProgramDetailComponent } from './programs/program-detail/program-detail.component';
 import { ProgramListComponent } from './programs/program-list/program-list.component';
-import { ProgramItemComponent } from './programs/program-list/program-item/program-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -46,9 +44,7 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     ProgramsComponent,
-    ProgramDetailComponent,
     ProgramListComponent,
-    ProgramItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     CalendarComponent,
