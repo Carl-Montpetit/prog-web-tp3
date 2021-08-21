960330e91d9f16f9ecb1a7113b254e0c5fea7f56
Carl <montpetit.carl@icloud.com>
2021-08-21 00:44:35 -0400

    The console force me to push... lol

--- src/app/accounts/create-account/create-account.component.ts
+++ src/app/accounts/create-account/create-account.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from "@angular/forms";
 })
 export class CreateAccountComponent implements OnInit {
   @ViewChild('f') createAccountForm: NgForm;
+  regEx = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
   private account = {
     firstName: '',
@@ -31,10 +32,22 @@ export class CreateAccountComponent implements OnInit {
   onSubmit(form: NgForm): void {
     this.account.firstName = this.createAccountForm.value.firstName;
     this.account.lastName = this.createAccountForm.value.lastName;
-    this.account.birthday = this.createAccountForm.value.birthday;
+    this.account.birthday = this.createAccountForm.value.date;
     this.account.email = this.createAccountForm.value.email;
     this.account.pw1 = this.createAccountForm.value.pw1;
     this.account.pw2 = this.createAccountForm.value.pw2;
     console.log(form);
+    console.log(this.account.firstName);
+    console.log(this.account.lastName);
+    console.log(this.account.birthday);
+    console.log(this.account.email);
+    console.log(this.account.pw1);
+    console.log(this.account.pw2);
+  }
+
+  validateFirstNameFormat(): boolean {
+    if (this.account.firstName.length >= 3 && this.account.firstName.length <= 30) {
+      return true;
+    }
   }
 }
