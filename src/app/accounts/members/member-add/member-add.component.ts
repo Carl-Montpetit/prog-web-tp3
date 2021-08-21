1eb614f7cfd20162b29a072dc912b8bfa17d88e2
Matthew <jovani.matthew@courrier.uqam.ca>
2021-08-21 15:00:21 -0400

    AddMember fonctionne

--- src/app/accounts/members/member-add/member-add.component.ts
+++ src/app/accounts/members/member-add/member-add.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MembersService } from '../members.service';
 
 @Component({
   selector: 'app-member-add',
@@ -8,19 +8,30 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./member-add.component.css']
 })
 export class MemberAddComponent implements OnInit {
+  @ViewChild('alert', {static: true}) alert: ElementRef;
+
+  submit = false;
+
   newMemberForm = new FormGroup ({
-    firstName: new FormControl('', Validators.required),
+    firstName: new FormControl(''),
     lastName: new FormControl(''),
     dateOfBirth: new FormControl(''),
   });
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    console.warn(this.newMemberForm.value);
+    this.memberService.addNewMember(this.newMemberForm.value.firstName, this.newMemberForm.value.lastName, this.newMemberForm.value.dateOfBirth)
+    this.submit = true
+    return this.submit;
+  }
+
+  closeAlert() {
+    this.submit = false;
+    this.alert.nativeElement.classList.remove('show');
   }
 
 }
--- src/app/accounts/members/members.service.ts
+++ src/app/accounts/members/members.service.ts
@@ -5,15 +5,15 @@ import { Children } from './member-model';
   providedIn: 'root'
 })
 export class MembersService {
-  static members = [    //TODO : delete 'new' & allow user to add to empty array like in Shopping Cart
-    new Children('Raven', 'Darkholme', '2003'),
-    new Children('Erik', 'Lehnsherr', '2009'),
-    new Children('Kurt', 'Wagner', '2016'),
-  ];
+  static members = [];
 
   constructor() { }
 
   getChildren() {
     return MembersService.members;
+  }
+
+  addNewMember(firstName, lastName, birthday) {
+    MembersService.members.push(new Children(firstName, lastName, birthday));
   }
 }
